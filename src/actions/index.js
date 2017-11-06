f0c3a3c088cb19f4017825b62b9507ec32853190
Gergo Nagy <guzmop@gmail.com>
2017-11-06 16:27:43 +0000

    api req

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,14 @@
-import API_KEY from '../../apiKey'
+import API_KEY from '../../apiKey';
+import axios from 'axios'
 
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},uk`
+    const request = axios.get(url)
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
